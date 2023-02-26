14cfd88da44ec53fb3edb6861cdf98071dbdcce9
黃祈瀚 <eric20645@gmail.com>
2023-02-26 22:36:56 +0800

    加入專案檔案。

---
+++ Entityhomework/Models/ContactsTable.cs
@@ -0,0 +1,27 @@
+namespace Entityhomework.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("ContactsTable")]
+    public partial class ContactsTable
+    {
+        [StringLength(50)]
+        public string Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        public int Total { get; set; }
+
+        public int Price { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string category { get; set; }
+    }
+}
